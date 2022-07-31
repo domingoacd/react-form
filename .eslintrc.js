227b28a8e1603fdb4ffef00fdf1355cc42e68ced
Domingo Cabeza <domingoacd1@gmail.com>
2022-07-31 11:48:21 -0400

    testing lint staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
     'plugin:security/recommended',
+    'plugin:import/typescript',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
@@ -17,11 +19,13 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'security'
-  ],
+  plugins: ['react', '@typescript-eslint', 'security'],
   rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { ts: 'never', tsx: 'never' },
+    ],
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
   },
 };
